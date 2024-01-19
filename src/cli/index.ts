74ed8305527cf2ddbe6e05b4beddcab1ecb07c2d
Пронин Роман Андреевич <ra.pronin@severstal.com>
2024-01-19 13:59:04 +0300

    feat: реализует автоматическое добавление команд

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,3 +2,5 @@ export { CLIApp } from './app/cli-app.js';
 export { HelpCommand } from './command/help.command.js';
 export { VersionCommand } from './command/version.command.js';
 export { ImportCommand } from './command/import.command.js';
+export { COMMAND_PATH_TEMPLATE } from './const.js';
+export type { ICommand } from './interface/command.interface.js';
